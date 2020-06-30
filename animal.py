92a70e3859ad624f14f3adb90d974721ed7543c9
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-06-30 19:00:47 +0530

    added dog feature

--- animal.py
+++ animal.py
@@ -1,7 +1,17 @@
+import sys
+
+
 def default():
     # This is the default case
     print("Hello!")
 
-if __name__ == "__main__":
-    default()
 
+def dog():
+    print("Woof!")
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == 'dog':
+        dog()
+    else:
+        default()
