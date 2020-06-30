4503632bcf6a01a088f5d0dca6636e7a37164903
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-06-30 18:56:54 +0530

    added cat feature

--- animal.py
+++ animal.py
@@ -1,7 +1,16 @@
+import sys
+
+
 def default():
     # This is the default case
     print("Hello!")
 
-if __name__ == "__main__":
-    default()
 
+def cat():
+    print("Meow!")
+
+if __name__ == "__main__":
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
